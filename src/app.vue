7bee0f11871db5308c71eec5fe1ec694affe5a08
Emilia <81705094+isemilia@users.noreply.github.com>
2024-09-12 16:31:02 +0500

    Adjust base styles

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
-        <div>nothing yet</div>
+    <div class='main'>
+        <aside>sidebar</aside>
+        <main>content</main>
     </div>
 </template>
 
 <script setup lang='ts'></script>
 
-<style scoped></style>
+<style scoped>
+.main {
+    height: 100%;
+    display: flex;
+}
+</style>
